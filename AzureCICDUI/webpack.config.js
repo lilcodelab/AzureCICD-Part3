0a91cadceb39cd5890bbd069178b86028ee621ad
David <david.koncic@littlecode.com>
2024-01-22 23:47:30 +0100

    safelisting purgecss

--- AzureCICDUI/webpack.config.js
+++ AzureCICDUI/webpack.config.js
@@ -7,10 +7,16 @@ const path = require('path');
 
 module.exports = {
   plugins: [
-    new PurgeCSSPlugin({
-      paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, { nodir: true }),
-      // Add other PurgeCSS options as needed
-    }),
+     new PurgeCSSPlugin({
+       paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, { nodir: true }),
+       safelist: {
+        standard: [
+          /^p-/,      // Safelists all classes starting with 'p-' (for PrimeNG)
+          /.*ng-.*/,  // Safelists any class that starts with 'ng-'
+        ],
+
+      }
+     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static' // This opens an HTML file with the bundle report
     })

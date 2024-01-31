3d9a1c36fdb85d8e7319d83f3217af597035ad2c
David <david.koncic@littlecode.com>
2024-01-31 08:46:43 +0100

    import environment bugfix

--- AzureCICDUI/src/services/user.service.ts
+++ AzureCICDUI/src/services/user.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { User } from '../models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(private http: HttpClient) { }
-
+  //naznačit u blog postu da se obavezno mora importat environment.ts a ne devleopment!
+  //jer se radi file replacements, bez toga se neće moć napraviti
   getAll(): Observable<User[]> {
     return this.http.get<User[]>(`${environment.apiUrl}/api/user/GetAll`);
   }

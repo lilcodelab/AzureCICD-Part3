4c8288679bf89c6aab27d0ecc09b2c0b4089a8e3
David <david.koncic@littlecode.com>
2024-02-05 12:50:57 +0100

    reverting all

--- AzureCICDUI/src/environments/environment.ts
+++ AzureCICDUI/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://myfeaturesdev.azurewebsites.net/'
-  //apiUrl: 'https://myfeatures.azurewebsites.net/'
+  apiUrl: 'https://myfeatures.azurewebsites.net/'
 };
